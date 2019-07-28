fe525ee665a8b3c569b78700b8d09428d07f587a
Effekseer <effekseer@gmail.com>
2019-07-28 11:01:00 +0900

    Updated Effekseer

--- Players/Cocos2d-x_v3/EffekseerRendererNative.h
+++ Players/Cocos2d-x_v3/EffekseerRendererNative.h
@@ -2838,6 +2838,8 @@ public:
 		, m_ringBufferOffset(0)
 		, m_ringBufferData(NULL)
 	{
+		// reserve buffers
+		instances.reserve(m_renderer->GetSquareMaxCount());
 	}
 
 	virtual ~SpriteRendererBase()
